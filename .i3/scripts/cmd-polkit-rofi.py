e8c6286b2cc9a8586b03fec57b825074968bf6d8
OmarCastro <omar.castro.360@gmail.com>
2024-06-22 23:58:16 +0100

    use custom rofi polkit agent

---
+++ .i3/scripts/cmd-polkit-rofi.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import json
+import sys
+import subprocess
+
+message = ""
+rofi_message = message
+prompt = "password: "
+
+def open_rofi():
+    cmd = ['rofi', '-password', '-dmenu', '-markup', '-no-fixed-num-lines', '-p', prompt, '-mesg', rofi_message ]
+    p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, err = p.communicate();
+    if out == b'':
+        print( """{"action": "cancel"}""" )
+        exit(0)
+    else:
+        print( """{"action": "authenticate", "password": "%s"}""" % str(out, "utf-8").rstrip("\n"), flush=True)
+
+
+for line in sys.stdin:
+    jsonObj = json.loads(line)
+    if "action" not in jsonObj :
+        continue
+    elif jsonObj["action"] == "request password":
+        if "prompt" in jsonObj:
+            prompt = jsonObj["prompt"]
+        if "message" in jsonObj:
+            message = jsonObj["message"]
+            rofi_message = message
+        if "polkit action" in jsonObj and jsonObj["polkit action"] != None:
+            polkit_action = jsonObj["polkit action"]
+            rofi_message = rofi_message + "\n\n"
+            if "description" in polkit_action: rofi_message = rofi_message + "\n<b>    Action:</b> " + polkit_action["description"]
+            if "id"          in polkit_action: rofi_message = rofi_message + "\n<b> Action ID:</b> " + polkit_action["id"] 
+            if "vendor name" in polkit_action: rofi_message = rofi_message + "\n<b>    Vendor:</b> " + polkit_action["vendor name"] 
+            if "vendor url"  in polkit_action: rofi_message = rofi_message + "\n<b>Vendor URL:</b> " + polkit_action["vendor url"]
+        open_rofi()
+
+
+
+
+
