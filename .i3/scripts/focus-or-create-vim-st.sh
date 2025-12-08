2de026efbd78ea944a5d2f22b4518f369ae1740e
OmarCastro <omar.castro.360@gmail.com>
2025-12-08 17:00:12 +0000

    replace vscodium with vim for coding

---
+++ .i3/scripts/focus-or-create-vim-st.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+active_class_name=$(xprop -id $(xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5) WM_CLASS | cut -d '"' -f 4)
+window_class_name="st-vim"
+font="Roboto Mono for powerline:pixelsize=16"
+
+# in non interactive shell PATH is not defined for ~/bin and ~/.local/bin
+if ! [ ":$PATH:" == *":$HOME/bin:"* ]; then PATH=$HOME/bin:$PATH:$HOME/.local/bin; fi
+
+function open_new_terminal(){
+	exec st -c $window_class_name -f "$font" bash -i -e -c vim
+}
+
+if [[ $active_class_name == $window_class_name ]]
+then
+	open_new_terminal
+else
+	i3-msg -s /run/user/$(id -u)/i3/ipc-socket.* '[class="^'$window_class_name'"] focus' || open_new_terminal
+fi
--- .i3/scripts/rofi-focus-or-launch.sh
+++ .i3/scripts/rofi-focus-or-launch.sh
@@ -7,7 +7,7 @@ read -r -d '' CONFIG << EOM
      f     |  Firefox              |  firefox                   |  firefox
      g     |  Chromium             |  Chromium                  |  chromium
      e     |  Caja file manager    |  Caja                      |  caja
-     v     |  VS Codium            |  VSCodium                  |  ~/bin/codium
+     v     |  Vim                  |  st-vim                    |  ~/.i3/scripts/focus-or-create-vim-st.sh
      u     |  Sublime Text         |  Subl                      |  subl
      t     |  Teams                |  Microsoft Teams - Preview |  teams
      p     |  Postman              |  Postman                   |  ~/apps/Postman/Postman
