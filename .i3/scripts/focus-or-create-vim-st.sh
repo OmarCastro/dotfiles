db05140eb0fc32302ebf0688724204ed9a22cfb9
OmarCastro <omar.castro.360@gmail.com>
2026-01-01 22:22:06 +0000

    use fira code for vim

--- .i3/scripts/focus-or-create-vim-st.sh
+++ .i3/scripts/focus-or-create-vim-st.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 active_class_name=$(xprop -id $(xprop -root _NET_ACTIVE_WINDOW | cut -d ' ' -f 5) WM_CLASS | cut -d '"' -f 4)
 window_class_name="st-vim"
-font="Roboto Mono for powerline:pixelsize=16"
+font="Fira Code:pixelsize=18"
 
 # in non interactive shell PATH is not defined for ~/bin and ~/.local/bin
 if ! [ ":$PATH:" == *":$HOME/bin:"* ]; then PATH=$HOME/bin:$PATH:$HOME/.local/bin; fi
